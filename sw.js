6618031d46ad4ed16b9fc4c357d22934a2798e14
jhonfredy2710-bit <jhonfredy2710@gmail.com>
2026-09-05 14:34:31 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-// Service worker mínimo, requerido por PWABuilder para el empaquetado Android.
-// No cachea nada: solo deja pasar las peticiones normales a la red.
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
